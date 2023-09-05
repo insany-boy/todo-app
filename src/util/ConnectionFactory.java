92c5924bd41a381d4e19c49d4af9f6e5b7445141
insany-boy <140752476+insany-boy@users.noreply.github.com>
2023-09-05 18:05:09 -0300

    Add files via upload

---
+++ src/util/ConnectionFactory.java
@@ -0,0 +1,14 @@
+
+package util;
+
+/**
+ *
+ * @author kauan
+ */
+public class ConnectionFactory {
+    
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/todo-app";
+    public static final String USER = "root";
+    public static final String PASS = "";
+}
